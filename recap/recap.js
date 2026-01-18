94cd26e10e6552dac101d8b9ba1c661f218b779b
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-18 21:49:36 +0600

    addad srcond error solve in money master project

--- recap/recap.js
+++ recap/recap.js
@@ -2,7 +2,7 @@ const calculateExpense = () => {
   const income = document.querySelector("#income").value;
   const food = document.querySelector("#food").value;
   const rent = document.querySelector("#rent").value;
-  const clothes = document.querySelectorAll("clothes").value;
+  const clothes = document.querySelector("#clothes").value;
 
   if (
     income < 0 ||
@@ -21,7 +21,7 @@ const calculateExpense = () => {
   const expense = parseInt(food) + parseInt(rent) + parseInt(clothes);
 
   // calculate balance
-  const balance = parseInt(income.value) - expense;
+  const balance = parseInt(income) - expense;
   //   validate income
   if (expense > income.value) {
     alert("Expenses cannot be more than income");
