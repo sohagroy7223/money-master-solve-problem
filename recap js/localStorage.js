4b669d67282c41d7c5e756401a5332715c0bc525
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-20 00:12:57 +0600

    addad how to setlocal storage

---
+++ recap js/localStorage.js
@@ -0,0 +1,8 @@
+const HandleBtn = () => {
+  const name = document.getElementById("name").value;
+  const id = document.getElementById("id").value;
+  //   localStorage.setItem(id, name);
+  const data = { id, name };
+  //   localStorage.setItem(id, data); //get object object
+  localStorage.setItem(id, JSON.stringify(data));
+};
