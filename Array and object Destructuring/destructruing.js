dd33f1f3c70672d75181558a92c76f8292b1a8d5
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-19 21:27:08 +0600

    addad destructuring object

--- Array and object Destructuring/destructruing.js
+++ Array and object Destructuring/destructruing.js
@@ -1,5 +1,19 @@
+// destructuring array variable don't matter, it matter index***
+
 const friends = ["akash", "ripon", "sagor", "sumon", "maha"];
 
 const [item1, item2, item3, item4, item5] = friends;
 
-console.log(item5);
+// console.log(item5);
+
+// destructuring object it matter matter property name *****
+
+const person = {
+  name: "sohag",
+  age: 20,
+  brother: ["sobuj", "samor"],
+  country: "Bangladesh",
+};
+
+const { name, country, brother, age } = person;
+console.log(brother);
